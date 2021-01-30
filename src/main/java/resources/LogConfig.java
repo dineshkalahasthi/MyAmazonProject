3200b47dcfafabfd5726e0d106b7d56b5a77e40c
dineshkalahasthi <kalahasthi.dinesh10@gmail.com>
2021-01-30 19:29:11 +0530

    commit before push

--- src/main/java/resources/LogConfig.java
+++ src/main/java/resources/LogConfig.java
@@ -12,7 +12,7 @@ public class LogConfig {
 	static {
 		StatusLogger.getLogger().setLevel(Level.OFF);
 	    System.out.println(LogConfig.class.getResource("/resources/log4j2.xml").getPath());
-		String configPath=LogConfig.class.getResource("/resources/log4j2.xml").getPath();
+		String configPath=LogConfig.class.getResource("/utilities/log4j2.xml").getPath();
 		//System.out.println(configPath);
 		//String configPath=LogConfig.class.getResource("/log4j2.properties").getPath();
 		((LoggerContext) LogManager.getContext(false)).setConfigLocation(new File(configPath).toURI());
